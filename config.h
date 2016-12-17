073d1c7c009c8d448fcbe0f4ca5a562775605047
John Anthony <john@jo.hnanthony.com>
2016-12-17 23:19:07 +0000

    Updated config.h for new config style

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *colors[SchemeLast][3]      = {
+static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { "#ffffff", "#222222", "#444444" },
 	[SchemeSel] =  { "#ffffff", "#005577",  "#005577" },
