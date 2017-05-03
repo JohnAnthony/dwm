e0f33c4e7667a8179deb8b67abf8a81a5566d02e
John Anthony <john@jo.hnanthony.com>
2017-05-03 15:27:30 +0100

    Status update now updates all monitors

--- dwm.c
+++ dwm.c
@@ -2028,9 +2028,12 @@ updatetitle(Client *c)
 void
 updatestatus(void)
 {
+	Monitor *m;
+
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "dwm-"VERSION);
-	drawbar(selmon);
+	for (m = mons; m; m = m->next)
+		drawbar(m);
 }
 
 void
